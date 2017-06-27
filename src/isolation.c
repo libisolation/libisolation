80d98631871011e02cc6f4c294669667468a8e17
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2017-06-28 01:58:44 +0900

    [isl] update to changes in libhypervisor

--- src/isolation.c
+++ src/isolation.c
@@ -19,7 +19,7 @@
 typedef unsigned long ulong;
 
 static int
-load_elf(Elf64_Ehdr *ehdr)
+load_elf(vmm_vmid_t vm, Elf64_Ehdr *ehdr)
 {
   assert(IS_ELF(*ehdr));
 
@@ -76,21 +76,21 @@ load_elf(Elf64_Ehdr *ehdr)
     if (p[i].p_flags & PF_W) prot |= PROT_WRITE;
     if (p[i].p_flags & PF_R) prot |= PROT_READ;
 
-    vmm_memory_map(mem, vaddr, size, prot);
+    vmm_memory_map(vm, mem, vaddr, size, prot);
 
     memcpy((char *) mem + offset, (char *) ehdr + p[i].p_offset, p[i].p_filesz);
   }
 
   // // construct stack
   // do_mmap(STACK_TOP - STACK_SIZE, STACK_SIZE, PROT_READ | PROT_WRITE, LINUX_PROT_READ | LINUX_PROT_WRITE, LINUX_MAP_PRIVATE | LINUX_MAP_FIXED | LINUX_MAP_ANONYMOUS, -1, 0);
-  // vmm_write_register(VMM_X64_RSP, STACK_TOP);
-  // vmm_write_register(VMM_X64_RBP, STACK_TOP);
+  // vmm_set_register(vmid, cpuid, VMM_X64_RSP, STACK_TOP);
+  // vmm_set_register(vmid, cpuid, VMM_X64_RBP, STACK_TOP);
 
   return 0;
 }
 
 static int
-do_load(const char *elf_path)
+do_load(vmm_vmid_t vm, const char *elf_path)
 {
   int fd;
   if ((fd = open(elf_path, O_RDONLY)) < 0) {
@@ -110,7 +110,7 @@ do_load(const char *elf_path)
     return -1;
   }
   int err;
-  if ((err = load_elf((Elf64_Ehdr *) data)) < 0) {
+  if ((err = load_elf(vm, (Elf64_Ehdr *) data)) < 0) {
     printf("huee");
     return err;
   }
@@ -120,43 +120,46 @@ do_load(const char *elf_path)
   return 0;
 }
 
+static vmm_vmid_t vmid;
+static vmm_cpuid_t cpuid;
+
 isl_handle_t
 isl_open(const char *filename)
 {
-  if (vmm_create() < 0)
+  if (vmm_create(&vmid) < 0)
     return -1;
-  if (vmm_cpu_create() < 0)
-    return -1;
-
-  if (do_load(filename) < 0)
+  if (vmm_cpu_create(vmid, &cpuid) < 0)
     return -1;
 
-  vmm_cpu_write_register(VMM_X64_RAX, 0);
-  vmm_cpu_write_register(VMM_X64_RBX, 0);
-  vmm_cpu_write_register(VMM_X64_RCX, 0);
-  vmm_cpu_write_register(VMM_X64_RDX, 0);
-  vmm_cpu_write_register(VMM_X64_RSI, 0);
-  vmm_cpu_write_register(VMM_X64_RDI, 0);
-  vmm_cpu_write_register(VMM_X64_R8, 0);
-  vmm_cpu_write_register(VMM_X64_R9, 0);
-  vmm_cpu_write_register(VMM_X64_R10, 0);
-  vmm_cpu_write_register(VMM_X64_R11, 0);
-  vmm_cpu_write_register(VMM_X64_R12, 0);
-  vmm_cpu_write_register(VMM_X64_R13, 0);
-  vmm_cpu_write_register(VMM_X64_R14, 0);
-  vmm_cpu_write_register(VMM_X64_R15, 0);
+  if (do_load(vmid, filename) < 0)
+    return -1;
 
-  vmm_cpu_write_register(VMM_X64_FS, 0);
-  vmm_cpu_write_register(VMM_X64_ES, 0);
-  vmm_cpu_write_register(VMM_X64_GS, 0);
-  vmm_cpu_write_register(VMM_X64_DS, 0);
-//  vmm_cpu_write_register(VMM_X64_CS, GSEL(SEG_CODE, 0));
-//  vmm_cpu_write_register(VMM_X64_DS, GSEL(SEG_DATA, 0));
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_RAX, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_RBX, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_RCX, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_RDX, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_RSI, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_RDI, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R8, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R9, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R10, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R11, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R12, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R13, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R14, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_R15, 0);
 
-//  vmm_cpu_write_register(VMM_X64_FS_BASE, 0);
-//  vmm_cpu_write_register(VMM_X64_GS_BASE, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_FS, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_ES, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_GS, 0);
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_DS, 0);
+//  vmm_cpu_set_register(vmid, cpuid, VMM_X64_CS, GSEL(SEG_CODE, 0));
+//  vmm_cpu_set_register(vmid, cpuid, VMM_X64_DS, GSEL(SEG_DATA, 0));
 
-  vmm_cpu_write_register(VMM_X64_LDTR, 0);
+//  vmm_cpu_set_register(vmid, cpuid, VMM_X64_FS_BASE, 0);
+//  vmm_cpu_set_register(vmid, cpuid, VMM_X64_GS_BASE, 0);
+
+  vmm_cpu_set_register(vmid, cpuid, VMM_X64_LDTR, 0);
 
   //init_fpu();
 
@@ -167,9 +170,9 @@ int
 isl_close(isl_handle_t handle)
 {
   assert(handle == 0);
-  if (vmm_cpu_destroy() < 0)
+  if (vmm_cpu_destroy(vmid) < 0)
     return -1;
-  if (vmm_destroy() < 0)
+  if (vmm_destroy(vmid) < 0)
     return -1;
   return 0;
 }
